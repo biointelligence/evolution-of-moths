a26a2bab80f6ae3738b14998a384f887b092eb6d
aiello <aiello@vertigo.com.br>
2020-08-07 14:14:33 -0300

    Spring Security.

--- src/main/java/com/evolutionofmoths/socket/config/WebSocketSecurityConfig.java
+++ src/main/java/com/evolutionofmoths/socket/config/WebSocketSecurityConfig.java
@@ -1,13 +1,15 @@
 package com.evolutionofmoths.socket.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
+@Configuration
 public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
 	@Override
 	protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-		messages.simpDestMatchers("/topic/**").authenticated().anyMessage().authenticated();
+		messages.anyMessage().authenticated();
 	}
 
 	@Override
