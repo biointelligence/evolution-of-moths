8ee95c1d97e1d606c0ed1bcca0e6836be85bdd34
aiello <aiello@vertigo.com.br>
2020-11-07 00:58:31 -0300

    Ajustes na falha de cálculo baseado na roleta.

--- src/main/java/com/evolutionofmoths/evolution/Population.java
+++ src/main/java/com/evolutionofmoths/evolution/Population.java
@@ -77,8 +77,10 @@ public abstract class Population {
 	 */
 	private void calculatePercentFitness(double totalFitness) {
 		for (Individual individual : individuals) {
-			if (totalFitness > 0)
-			individual.setPercentFitness((individual.getFitness() * 100) / totalFitness);
+			if (totalFitness > 0) {
+				individual.setPercentFitness((individual.getFitness() * 100) / totalFitness);
+			}
+			
 		}
 	}
 
